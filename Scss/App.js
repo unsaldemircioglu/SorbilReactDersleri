02daa9f2e2a5c20ab3152a783f8d6bbb2a7e30e5
Ünsal Demircioğlu <142059934+unsaldemircioglu@users.noreply.github.com>
2025-10-01 22:46:36 +0100

    Add files via upload

---
+++ Scss/App.js
@@ -0,0 +1,26 @@
+import "./style.scss";
+
+function App() {
+  return (
+         <main>
+                  <header>
+                           <div className='header'>
+                                    <h1>Scss Kullanımı</h1>
+                                    <p>Lorem ipsum lorem ipsum</p>
+                           </div>
+                  </header>
+                  <aside>
+                           <div className='aside'>
+                                    <span>Scss</span>
+                           </div>
+                  </aside>
+                  <article>
+                           <div className='article'>
+                                       <span>Renk Paleti</span>
+                           </div>
+                  </article>
+         </main>
+  );
+}
+
+export default App;
